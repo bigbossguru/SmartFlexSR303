b124233044a84bb9dac2658d4c7ad8fbc3a0d16a
Eldar Gazizov <egazizov@elektroline.cz>
2021-06-16 15:48:26 +0200

    connect submodule changelogger and create batch file for run script

--- _CHANGELOG_RUNNER.bat
+++ _CHANGELOG_RUNNER.bat
@@ -1,3 +1,3 @@
 git log --date=format:"%%d-%%m-%%Y" --pretty="## [%%h] - %%ad - %%s" > CHANGELOG_orig.md
-python change_log_creater.py
-mv CHANGELOG.md ../
+python ./Changelog-generator/change_log_creater.py
+mv CHANGELOG.md
